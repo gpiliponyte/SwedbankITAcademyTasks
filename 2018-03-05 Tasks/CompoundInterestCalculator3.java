904db68c3b77734f2d2ec700bcb5969a812bd636
Greta Piliponytė <g.piliponyte97@gmail.com>
2018-03-06 22:29:49 +0200

    refactoring

---
+++ 2018-03-05 Tasks/CompoundInterestCalculator3.java
@@ -0,0 +1,90 @@
+package lt.swedbank.interestcalculator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class CompoundInterestCalculator3 {
+
+    private static final int PERCENT = 100;
+
+    public static void main(String[] args) {
+
+        double originalAmount;
+        int annualInterestRate;
+        int periodLengthInYears;
+        int frequency;
+        String compoundFrequency;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        originalAmount = scanner.nextDouble();
+
+        System.out.print("Interest rate (%): ");
+        annualInterestRate = scanner.nextInt();
+
+        System.out.print("Period length (years): ");
+        periodLengthInYears = scanner.nextInt();
+
+        System.out.print("Compound frequency: ");
+        compoundFrequency = scanner.next();
+
+        scanner.close();
+
+        frequency = findFrequency(compoundFrequency);
+
+        double[] intermediateAmounts = new double[frequency * periodLengthInYears];
+
+        for (int i = 1; i <= periodLengthInYears * frequency; i++) {
+
+            intermediateAmounts[i - 1] = calculateNewInterestAmount(annualInterestRate, originalAmount, frequency);
+            originalAmount += intermediateAmounts[i - 1];
+
+            if (i % frequency == 0) {
+
+                System.out.printf("Interest amount after year %d : %.2f \n", i / frequency, calculateGatheredAmountAtYear(intermediateAmounts, i / frequency, frequency));
+            }
+
+        }
+        System.out.printf("Total amount: %.2f \n", originalAmount);
+
+        System.out.println(Arrays.toString(intermediateAmounts));
+
+    }
+
+    private static double calculateNewInterestAmount(int annualInterestRate, double originalAmount, int frequency) {
+
+        return (originalAmount * Math.pow(1 + ((double) annualInterestRate / PERCENT / frequency), frequency)) - originalAmount;
+    }
+
+
+    private static double calculateGatheredAmountAtYear(double[] intermediateAmounts, int year, int frequency) {
+
+        double gatheredAmountOfInterest = 0;
+
+        for (int i = 0; i < year * frequency; i++) {
+            gatheredAmountOfInterest += intermediateAmounts[i];
+        }
+
+        return gatheredAmountOfInterest;
+    }
+
+
+    private static int findFrequency(String frequency) {
+
+        switch (frequency) {
+            case "D":
+                return 365;
+            case "W":
+                return 52;
+            case "M":
+                return 12;
+            case "Q":
+                return 4;
+            case "H":
+                return 2;
+            default:
+                return 1;
+        }
+    }
+}
---
+++ 2018-03-05 Tasks/CompoundInterestCalculator5.java
@@ -0,0 +1,133 @@
+package lt.swedbank.interestcalculator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class CompoundInterestCalculator5 {
+
+    private static final int PERCENT = 100;
+
+    public static void main(String[] args) {
+
+        String input;
+        double originalAmount;
+        int numberOfInterestRates = 0;
+        double annualInterestRates[] = new double[numberOfInterestRates];
+        double interestRate;
+        int periodLengthInYears;
+        int frequency;
+        String compoundFrequency;
+        double newAmount;
+        double copyOfAnnualInterestAmounts[];
+        double yearlyInterestMatrix[][];
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            try {
+                System.out.print("Amount: ");
+                input = scanner.next();
+                originalAmount = Double.parseDouble(input);
+                if (originalAmount > 0) {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+            }
+            System.out.println("Invalid input! Try again!");
+        }
+
+
+        do {
+            try {
+                System.out.print("Interest rate (%): ");
+                input = scanner.next();
+                interestRate = Double.parseDouble(input);
+                if (interestRate == 0) {
+                    break;
+                } else if (interestRate > 0 && interestRate <= 100) {
+                    numberOfInterestRates++;
+                    copyOfAnnualInterestAmounts = new double[numberOfInterestRates];
+                    System.arraycopy(annualInterestRates, 0, copyOfAnnualInterestAmounts, 0, annualInterestRates.length);
+                    annualInterestRates = copyOfAnnualInterestAmounts;
+                    annualInterestRates[numberOfInterestRates - 1] = interestRate;
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+
+            }
+            System.out.println("Invalid input! Try again!");
+        } while (true);
+
+
+        while (true) {
+            try {
+                System.out.print("Period length (years): ");
+                input = scanner.next();
+                periodLengthInYears = Integer.parseInt(input);
+                if (periodLengthInYears > 0) break;
+            } catch (NumberFormatException e) {
+            }
+            System.out.println("Invalid input! Try again!");
+        }
+
+
+        yearlyInterestMatrix = new double[numberOfInterestRates][periodLengthInYears];
+
+
+        while (true) {
+            System.out.print("Compound frequency: ");
+            compoundFrequency = scanner.next();
+            if (Arrays.asList(new String[]{"D", "W", "M", "Q", "H", "Y"}).contains(compoundFrequency)) {
+                break;
+            }
+            System.out.println("Invalid input! Try again!");
+        }
+
+
+        scanner.close();
+
+        frequency = findFrequency(compoundFrequency);
+
+
+        for (int i = 0; i < numberOfInterestRates; i++) {
+            newAmount = originalAmount;
+            for (int j = 0; j < periodLengthInYears; j++) {
+                yearlyInterestMatrix[i][j] = calculateNewInterestAmount(annualInterestRates[i], newAmount, frequency);
+                newAmount += yearlyInterestMatrix[i][j];
+            }
+        }
+
+
+        for (double[] row : yearlyInterestMatrix) {
+            for (double element : row) {
+                System.out.printf("%.2f ", element);
+            }
+            System.out.println();
+        }
+    }
+
+
+    private static double calculateNewInterestAmount(double annualInterestRate, double originalAmount, int frequency) {
+        return (originalAmount * Math.pow(1 + (annualInterestRate / PERCENT / frequency), frequency)) - originalAmount;
+    }
+
+
+    private static int findFrequency(String frequency) {
+        switch (frequency) {
+            case "D":
+                return 365;
+            case "W":
+                return 52;
+            case "M":
+                return 12;
+            case "Q":
+                return 4;
+            case "H":
+                return 2;
+            default:
+                return 1;
+        }
+    }
+}
